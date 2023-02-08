0de78245d1e848fe81e5616cc6799da4e44c9785
Raphaël Vinot <raphael@vinot.info>
2023-02-08 16:12:07 +0100

    new: Very basic enqueuing

--- webmonitoring/webmonitoring.py
+++ webmonitoring/webmonitoring.py
@@ -2,6 +2,12 @@
 
 import logging
 
+from uuid import uuid4
+
+from datetime import datetime, timedelta
+from typing import MutableMapping, Any, Optional, Union
+
+from pylookyloo import Lookyloo
 from redis import ConnectionPool, Redis
 from redis.connection import UnixDomainSocketConnection
 
@@ -16,6 +22,21 @@ class Monitoring():
 
         self.redis_pool: ConnectionPool = ConnectionPool(connection_class=UnixDomainSocketConnection,
                                                          path=get_socket_path('cache'), decode_responses=True)
+        # TODO: configurable URL, useragent
+        self.lookyloo = Lookyloo()
+
+        # Pop a range of keys
+        # local elems = redis.call('ZRANGE', key, 0, now, 'BYSCORE')
+        lua = """
+        local key = KEYS[1]
+        local now = ARGV[1]
+        local elems = redis.call('ZRANGE', key, 0, now, 'BYSCORE')
+        if elems.length then
+          redis.call('ZREM', key, unpack(elems))
+        end
+        return elems
+        """
+        self.redis_zpoprangebyscore = self.redis.register_script(lua)
 
     @property
     def redis(self):
@@ -23,3 +44,52 @@ class Monitoring():
 
     def check_redis_up(self):
         return self.redis.ping()
+
+    def monitor(self, capture_settings: MutableMapping[str, Any], /, frequency: str, *,
+                expire_at: Optional[Union[datetime, str, int, float]]=None, collection: Optional[str]=None):
+        monitor_uuid = str(uuid4())
+        p = self.redis.pipeline()
+        p.hset(f'{monitor_uuid}:capture_settings', mapping=capture_settings)
+        p.set(f'{monitor_uuid}:frequency', frequency)
+        if collection:
+            p.set(f'{monitor_uuid}:collection', collection)
+        if expire_at:
+            if isinstance(expire_at, (str, int, float)):
+                _expire = float(expire_at)
+            if isinstance(expire_at, datetime):
+                _expire = expire_at.timestamp()
+            if _expire < datetime.now().timestamp():
+                # The expiration time is in the past.
+                raise Exception('Expiration time in the past.')
+            p.set(f'{monitor_uuid}:expire', _expire)
+        p.sadd('monitored', monitor_uuid)
+        p.execute()
+        return monitor_uuid
+
+    def update_monitoring_queue(self):
+        for monitor_uuid in self.redis.smembers('monitored'):
+            if self.redis.zscore('monitoring_queue', monitor_uuid):
+                # don't do anythfing, let the next capture happen
+                continue
+            _expire = self.redis.get(f'{monitor_uuid}:expire')
+            if _expire and datetime.now().timestamp() < _expire:
+                # Monitoring expired
+                self.redis.smove('monitored', 'expired_monitored', monitor_uuid)
+                continue
+            freq = self.redis.get(f'{monitor_uuid}:frequency')
+            # WIP, few hardcoded values - later, use the cron format too
+            next_run = {monitor_uuid: 0}
+            if freq == 'hourly':
+                next_run[monitor_uuid] = (datetime.now() + timedelta(hours=1)).timestamp()
+            elif freq == 'daily':
+                next_run[monitor_uuid] = (datetime.now() + timedelta(days=1)).timestamp()
+            else:
+                raise Exception(f'Frequency unsupported: {freq}')
+            self.redis.zadd('monitoring_queue', mapping=next_run)
+
+    def process_monitoring_queue(self):
+        now = datetime.now().timestamp()
+        for monitor_uuid in self.redis_zpoprangebyscore(keys=['monitoring_queue'], args=[now]):
+            settings = self.redis.hgetall(f'{monitor_uuid}:capture_settings')
+            new_capture = {self.lookyloo.enqueue(*settings): now}
+            self.redis.zadd(f'{monitor_uuid}:captures', mapping=new_capture)
--- website/web/__init__.py
+++ website/web/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 from importlib.metadata import version
-from flask import Flask
-from flask_restx import Api, Resource  # type: ignore
+from flask import Flask, request
+from flask_restx import Api, Resource, fields  # type: ignore
 
 from webmonitoring.webmonitoring import Monitoring
 
@@ -28,3 +28,32 @@ class RedisUp(Resource):
 
     def get(self):
         return monitoring.check_redis_up()
+
+
+capture_settings_mapping = api.model('CaptureSettings', {
+    'url': fields.Url(description="The URL to capture")
+})
+
+
+monitor_fields_post = api.model('MonitorFieldsPost', {
+    'capture_settings': fields.Nested(capture_settings_mapping, description="The capture settings"),
+    'frequency': fields.String('The frequency of the capture'),
+    'expire_at': fields.String('When the monitoring expires, empty means never'),
+    'collection': fields.String('The name of the collection')
+})
+
+
+@api.route('/monitor')
+@api.doc(description='Add a capture in the monitoring')
+class Monitor(Resource):
+
+    @api.doc(body=monitor_fields_post)
+    def post(self):
+        monit = request.get_json(force=True)  # type: ignore
+        print(monit)
+        monitor_uuid = monitoring.monitor(monit['capture_settings'], frequency=monit['frequency'],
+                                          expire_at=monit.get('expire_at'), collection=monit.get('collection'))
+        # TODO: get that somewhere else.
+        monitoring.update_monitoring_queue()
+        monitoring.process_monitoring_queue()
+        return monitor_uuid
