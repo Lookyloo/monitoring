675c78ff3b886238423546fff86b06fa68ea0655
Raphaël Vinot <raphael@vinot.info>
2023-06-14 16:15:33 +0200

    chg: Improve email notification

--- webmonitoring/mail.py
+++ webmonitoring/mail.py
@@ -1,5 +1,6 @@
 import logging
 import smtplib
+import ssl
 
 from email.message import EmailMessage
 
@@ -18,13 +19,18 @@ class Mail:
         logger.setLevel(get_config('generic', 'loglevel'))
 
         try:
-            server = smtplib.SMTP(host=email_config['smtp_host'], port=email_config['smtp_port'])
-            if smtp_auth['auth']:
-                server.login(smtp_auth['smtp_user'], smtp_auth['smtp_pass'])
-                if smtp_auth['smtp_use_tls']:
-                    server.starttls()
-            server.send_message(email)
-            server.quit()
+            with smtplib.SMTP(host=email_config['smtp_host'], port=email_config['smtp_port']) as server:
+                if smtp_auth['auth']:
+                    if smtp_auth['smtp_use_starttls']:
+                        if smtp_auth['verify_certificate'] is False:
+                            ssl_context = ssl.create_default_context()
+                            ssl_context.check_hostname = False
+                            ssl_context.verify_mode = ssl.CERT_NONE
+                            server.starttls(context=ssl_context)
+                        else:
+                            server.starttls()
+                        server.login(smtp_auth['smtp_user'], smtp_auth['smtp_pass'])
+                server.send_message(email)
         except (ConnectionRefusedError, smtplib.SMTPException) as e:
             logger.critical(f'Unable to send mail: {e}\n{email}')
             return False
